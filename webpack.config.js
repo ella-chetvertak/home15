4eef751b552385f1e1c43b39ec300d465556121f
ella-chetvertak <olegcetvertak037@gmail.com>
2022-08-17 18:04:17 +0300

    big css fixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,54 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './js/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './index.html'),
-            filename: 'index.html', 
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: './styles/main.css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.(sass|css|scss)$/,
-                use: [
-                  'style-loader',
-                  'css-loader',
-                  {
-                    loader: "postcss-loader",
-                  },
-                  'sass-loader',
-                ]
-            },
-        ],
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        open: true,
+        hot: true,
+        port: 8080,
     }
-}
+};
+
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  entry: './js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: './index.html'
+      }),
+      new MiniCssExtractPlugin({
+          filename: './styles/main.css'
+      })
+  ],
+  module: {
+      rules: [
+          {
+              test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+              type: 'asset/inline'
+          },
+          {
+              test: /\.html$/i,
+              loader: 'html-loader'
+          },
+          {
+              test: /\.css$/i,
+              use: [
+                MiniCssExtractPlugin.loader, 'css-loader'
+              ]
+          },
+          {
+              test: /\.scss$/i,
+              use: [
+                MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+              ]
+          }
+      ]
+  },
+  ...devServer(develop),
+});
